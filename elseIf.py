d622dfc04c4f9b6b2f40673d9a3c42976095cfd7
anmolrattanarora <91684721+anmolrattanarora@users.noreply.github.com>
2021-10-01 01:07:24 +0530

    Update elseIf.py

--- elseIf.py
+++ elseIf.py
@@ -1,8 +1,8 @@
-x = 32
-if x < 1 :
+x = 64
+if x < 10 :
     print('it is smaller than 1')
 elif x > 100 :
     print('it is greater than 100')
 else :
     print('it is smaller than 100')
-print('All Done') 
+print('All DONE') 
