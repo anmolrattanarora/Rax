9123d874207018c836e28c49edd34c21de5eff7d
tarusharora03 <58811191+tarusharora03@users.noreply.github.com>
2021-10-01 00:52:43 +0530

    Add files via upload

---
+++ dict2.py
@@ -0,0 +1,4 @@
+iii = {'rax' : 1 , 'alex' : 2 , 'nation' : 3 , 'roy' : 4}
+print(iii)
+iii['rax'] = iii['rax'] + 10
+print(iii)
---
+++ elseIf.py
@@ -0,0 +1,8 @@
+x = 32
+if x < 1 :
+    print('it is smaller than 1')
+elif x > 100 :
+    print('it is greater than 100')
+else :
+    print('it is smaller than 100')
+print('All Done') 
